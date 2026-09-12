f58ed0a5c9f3e1a32837cf9208d67a0ebedc6de0
unknown <yeanuryaj@gmail.com>
2026-09-12 15:42:58 +0600

    add banner sectiopn

--- src/components/Technologies/technologies.tsx
+++ src/components/Technologies/technologies.tsx
@@ -1,30 +1,5 @@
 
 
-// import { use } from "react";
-
-//    import type { Itechonologies } from "../../type/techonologiesType";
-
-
-// interface TechnologiesProps  {
-//      technologiesPromise: Promise<Itechonologies[]>;
-// }
-
-// const Technologies = ({ technologiesPromise }: TechnologiesProps) => {
-//   console.log(technologiesPromise);
-
-//   const technologies = use(technologiesPromise);
-
-//   console.log(technologies, "technologies");
-
-//   return (
-//     <div>
-      
-//     </div>
-//   );
-// };
-
-// export default Technologies;
-
 import { use, useState } from "react";
 import type { Itechonologies } from "../../type/techonologiesType";
 import TechnologyCard from "./TechnologyCard";
